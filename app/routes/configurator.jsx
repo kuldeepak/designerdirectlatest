d54175bacd12403b9ad87a9487d7360ca319190d
kuldeepak <kuldeepak05@gmail.com>
2026-03-20 11:12:21 +0530

    CHANGE EMAIL IPV6 NEW

--- app/routes/configurator.jsx
+++ app/routes/configurator.jsx
@@ -142,15 +142,26 @@ export async function action({ request }) {
 
     if (buyerName && productRef && designerEmail) {
       try {
+        // const transporter = nodemailer.createTransport({
+        //   host: "smtp.gmail.com",
+        //   port: 587,
+        //   secure: false,
+        //   auth: {
+        //     user: "kas.kuldeepakthakur@gmail.com",
+        //     pass: "ftkg shcr hbrl knpz"
+        //   },
+        //   ipVersion: 'ipv4'
+        // });
+
         const transporter = nodemailer.createTransport({
           host: "smtp.gmail.com",
           port: 587,
           secure: false,
+          family: 4,          // ✅ forces IPv4
           auth: {
             user: "kas.kuldeepakthakur@gmail.com",
             pass: "ftkg shcr hbrl knpz"
-          },
-          ipVersion: 'ipv4'
+          }
         });
 
 
@@ -161,11 +172,11 @@ export async function action({ request }) {
           cc: buyerEmail || undefined,
           subject: `Inquiry for ${productRef}`,
           text: `
-      Name: ${buyerName}
-      User Email: ${buyerEmail || "Not provided"}
-      Product: ${productRef}
-      Message: ${message || "No message"}
-`
+                  Name: ${buyerName}
+                  User Email: ${buyerEmail || "Not provided"}
+                  Product: ${productRef}
+                  Message: ${message || "No message"}
+            `
         })
           .then(() => {
             console.log("Inquiry email sent");
